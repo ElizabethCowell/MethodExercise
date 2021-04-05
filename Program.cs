c1b5f337d21c726736f3f0c04e7bc355e3638ae9
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-05 15:13:01 -0400

    completed first exercise

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        public static string Name()
+        {
+            string name = Console.ReadLine();
+            return name;
+        }
+        public static string Color()
+        {
+            string color = Console.ReadLine();
+            return color;
+        }
+        public static string Animal()
+        {
+            string animal = Console.ReadLine();
+            return animal;
+        }
+        public static string Band()
+        {
+            string band = Console.ReadLine();
+            return band;
+        }
+         static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Name();
+            Console.WriteLine("What is your favorite color?");
+            string color = Color();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Animal();
+            Console.WriteLine("What is your favorite band?");
+            string band = Band();
+
+            Console.WriteLine($"It was a warm and {color} day when I decided I wanted to check out the new hot spot in town, {band}. I was intrigued when my server introduced themselves as {name} {animal}.");
+
+         
+        }
+    }
+}
