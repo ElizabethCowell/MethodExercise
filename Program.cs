75d77c842a56125065ed4c8f5e949142904a2c91
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-05 15:38:01 -0400

    completed 2nd exercise

--- Program.cs
+++ Program.cs
@@ -24,6 +24,32 @@ namespace MethodExercise
             string band = Console.ReadLine();
             return band;
         }
+        public static int Addition(int x, int y)
+        {
+            int sum = x+y;
+            return sum;
+        }
+        public static int Subtraction(int x, int y)
+        {
+            int difference = x-y;
+            return difference;
+        }
+        public static int Multiply(int x, int y)
+        {
+            int total = x*y;
+            return total;
+        }
+        public static int Divide(int x, int y)
+        {
+            int equals = x / y;
+            return equals;
+        }
+        public static int Mod(int x, int y)
+        {
+            int remainder = x % y;
+            return remainder;
+        }
+            
          static void Main(string[] args)
         {
             Console.WriteLine("What is your name?");
@@ -37,6 +63,17 @@ namespace MethodExercise
 
             Console.WriteLine($"It was a warm and {color} day when I decided I wanted to check out the new hot spot in town, {band}. I was intrigued when my server introduced themselves as {name} {animal}.");
 
+           
+            int sum = Addition(6, 8);
+            Console.WriteLine(sum);
+            int diff = Subtraction(12, 4);
+            Console.WriteLine(diff);
+            int total = Multiply(5, 12);
+            Console.WriteLine(total);
+            int equals = Divide(12, 3);
+            Console.WriteLine(equals);
+            int remainder = Mod(14, 5);
+            Console.WriteLine(remainder);
          
         }
     }
